eb2ce1e20cc966299c5b30ec53c8cba60d2ba8ef
jungmin2213 <jungmin2213@hanyang.ac.kr>
2023-03-27 17:59:59 +0900

    first commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -54,7 +54,7 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                                 children: [
                                   TextSpan(
                                     //TODO : letter spacing 1.0, font size 25, color white, bold
-                                    text: 'to EOS chat',
+                                    text: isSignUpScreen? 'to EOS chat': 'back',
                                     style: TextStyle(
                                       letterSpacing: 1.0,
                                       fontSize: 25,
@@ -71,7 +71,7 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                         ),
                         Text(
                           //TODO : spacing 1.0, color white
-                          'Signup to Continue',
+                          isSignUpScreen? 'Signup to Continue' : 'Signin to Continue',
                           style: TextStyle(
                             letterSpacing: 1.0,
                             color: Colors.white,
@@ -82,13 +82,16 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                   ),
                 )
             ),
-            Positioned(
-              //TODO : top 150
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 500),
+                //TODO : top 150
                 top: 150.0,
-                child: Container(
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
                   //TODO : height 280.0, padding 모두 20, width 핸드폰 가로 길이 - 40
                   //TODO : margin 가로로 양쪽 20
-                  height: 280.0,
+                  height: isSignUpScreen?  280.0 : 250.0,
                   padding: EdgeInsets.all(20.0),
                   width: MediaQuery.of(context).size.width - 40,
                   margin: EdgeInsets.symmetric(horizontal: 20.0),
@@ -113,6 +116,11 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  isSignUpScreen = false;
+                                });
+                              },
                               child: Column(
                                 children: [
                                   Text(
@@ -121,23 +129,30 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                                     style: TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold,
+                                      decoration: isSignUpScreen? TextDecoration.underline : null,
                                       color: isSignUpScreen ? Palette.textColor1 : Palette.activeColor,
                                     ),
                                     //TODO : isSignupScreen이 false면 palette의 activeColor, true면 palette의 textcolor1
-
                                   ),
-                                  //TODO : isSignupScreen이 false일 때만 밑줄이 생기도록
-                                  Container(
-                                    //TODO : margin top만 3, height 2, width 55, color green
-                                    margin: EdgeInsets.only(top: 3.0),
-                                    height: 2.0,
-                                    width: isSignUpScreen ? 0 : 55.0,
-                                    color: isSignUpScreen ? Colors.transparent : Colors.green,
-                                  )
+                                  isSignUpScreen
+                                    ? SizedBox()
+                                    //TODO : isSignupScreen이 false일 때만 밑줄이 생기도록
+                                    : Container(
+                                      //TODO : margin top만 3, height 2, width 55, color green
+                                      margin: EdgeInsets.only(top: 3.0),
+                                      height: 2.0,
+                                      width: isSignUpScreen ? 0 : 55.0,
+                                      color: Colors.green,
+                                    )
                                 ],
                               ),
                             ),
                             GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isSignUpScreen = true;
+                                  });
+                                },
                                 child: Column(
                                   children: [
                                     Text(
@@ -147,29 +162,38 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                                       style: TextStyle(
                                         fontSize: 16,
                                         fontWeight: FontWeight.bold,
+                                        decoration: isSignUpScreen? TextDecoration.underline : null,
                                         color: isSignUpScreen ? Palette.activeColor : Palette.textColor1,
-
                                       ),
                                     ),
                                     //TODO : isSignUpScreen이 true일 때만 밑줄이 생기도록
-                                    Container(
-                                      //TODO : margin top만 3, height 2, width 55, color green
-                                      margin: EdgeInsets.only(top: 3),
-                                      height: 2,
-                                      width: isSignUpScreen ? 55.0 : 0,
-                                      color: isSignUpScreen ? Colors.green : Colors.transparent,
-                                    )
+
+                                    isSignUpScreen
+                                        ? Container(
+                                        //TODO : margin top만 3, height 2, width 55, color green
+                                        margin: EdgeInsets.only(top: 3),
+                                        height: 2,
+                                        width: isSignUpScreen ? 55.0 : 0,
+                                        color: isSignUpScreen ? Colors.green : Colors.transparent,
+                                        )
+                                        :SizedBox()
                                   ],
                                 )
                             )
                           ],
                         ),
-                        Container(
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                            curve: Curves.easeIn,
+                            margin: EdgeInsets.only(top: 20.0),
                             child: Form(
                               child: Column(
                                   children: [
+                                    isSignUpScreen ?
                                     TextFormField(
                                       decoration: InputDecoration(
+                                        hintText: 'User name',
+                                        hintStyle: TextStyle(color: Palette.iconColor),
                                         //TODO : prefix icon 원하는 icon 입력, color는 palette의 iconColor
                                         prefixIcon: Icon(Icons.email, color: Palette.iconColor),
                                         enabledBorder: OutlineInputBorder(
@@ -179,10 +203,54 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                                           borderRadius: BorderRadius.circular(35.0),
                                         ),
                                         focusedBorder: OutlineInputBorder(
-                                          //TODO : borderside 색은 palette의 textColot2
+                                          //TODO : borderside 색은 palette의 textColot1
                                           //TODO : border radius는 모두 35
-                                          borderSide: BorderSide(color: Palette.textColor2), // borderSide 설정
+                                          borderSide: BorderSide(color: Palette.textColor1), // borderSide 설정
                                           borderRadius: BorderRadius.circular(35.0),
+                                        ),
+                                      ),
+                                    )
+                                        : SizedBox(),
+                                    SizedBox(height: 8,),
+
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: 'email',
+                                        hintStyle: TextStyle(color: Palette.iconColor),
+                                        prefixIcon: Icon(Icons.email),
+                                        prefixIconColor: Palette.iconColor,
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(35),
+                                          borderSide: BorderSide(
+                                            color: Palette.textColor1
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(35),
+                                          borderSide: BorderSide(
+                                            color: Palette.textColor1
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 8,),
+
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: 'password',
+                                        hintStyle: TextStyle(color: Palette.iconColor),
+                                        prefixIcon: Icon(Icons.lock),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(35),
+                                          borderSide: BorderSide(
+                                            color: Palette.textColor1
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(35),
+                                          borderSide: BorderSide(
+                                            color: Palette.textColor1,
+                                          ),
                                         ),
                                       ),
                                     )
@@ -193,9 +261,74 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                       ]
                   ),
                 )
-            )
+            ),
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 500),
+                curve: Curves.easeIn,
+                top: isSignUpScreen? 390 : 340,
+                right: 0,
+                left: 0,
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(15.0),
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Colors.lightGreen, Colors.green],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight
+                              ),
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(color: Colors.white, width: 10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                spreadRadius: 1,
+                                blurRadius: 1,
+                                offset: Offset(0, 1)
+                              )
+                            ]
+                          ),
+                          child: Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white
+                          ),
+                        )
+                    ),
+                ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height - 125,
+              right: 0,
+              left: 0,
+              child: Column(
+                children: [
+                  Text('or Signin with'),
+                  SizedBox(height: 10),
+                  TextButton.icon(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      minimumSize: Size(155, 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Palette.googleColor,
+                    ),
+                    icon: Icon(Icons.add),
+                    label: Text('Google'),
+                  ),
+                ],
+              ),
+            ),
           ],
-        )
-    );
+        ),
+      );
   }
 }
